38721035576df2f8ee7982e0471a81ba41fa0595
Andy <andyhalim.az@gmail.com>
2024-05-08 07:45:01 +0700

    search

---
+++ search/lib/search.dart
@@ -0,0 +1,8 @@
+library search;
+ 
+export 'domain/usecases/movies/search_movies.dart';
+export 'presentation/provider/movies/movie_search_notifier.dart';
+export 'presentation/pages/movies/search_page.dart';
+export 'domain/usecases/tv_series/search_tv_series.dart';
+export 'presentation/provider/tv_series/tv_series_search_notifier.dart';
+export 'presentation/pages/tv_series/search_page.dart';
---
+++ search/test/domain/usecases/tv_series/search_tv_series_test.dart
@@ -0,0 +1,27 @@
+import 'package:core/domain/entities/tv_series.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:search/domain/usecases/tv_series/search_tv_series.dart';
+
+void main() {
+  late SearchTvSeries usecase;
+  late MockTvSeriesRepository mockTvSeriesRepository;
+
+  setUp(() {
+    mockTvSeriesRepository = MockTvSeriesRepository();
+    usecase = SearchTvSeries(mockTvSeriesRepository);
+  });
+
+  final tTvSeriesList = <TvSeries>[];
+  const tQuery = 'Dirty Linen';
+
+  test('should get list of tv series from the repository', () async {
+    // arrange
+    when(mockTvSeriesRepository.searchTvSeries(tQuery)).thenAnswer((_) async => Right(tTvSeriesList));
+    // act
+    final result = await usecase.execute(tQuery);
+    // assert
+    expect(result, Right(tTvSeriesList));
+  });
+}
