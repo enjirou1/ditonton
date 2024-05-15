0d601f31e809fd14266a88c308b29569c8e66d45
Andy <andyhalim.az@gmail.com>
2024-05-15 07:51:43 +0700

    encrypt

--- core/lib/data/datasources/db/database_helper.dart
+++ core/lib/data/datasources/db/database_helper.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 
 import 'package:core/data/models/movies/movie_table.dart';
 import 'package:core/data/models/tv_series/tv_series_table.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:core/utils/encrypt.dart';
+import 'package:sqflite_sqlcipher/sqflite.dart';
 
 class DatabaseHelper {
   static DatabaseHelper? _databaseHelper;
@@ -29,7 +30,12 @@ class DatabaseHelper {
     final path = await getDatabasesPath();
     final databasePath = '$path/ditonton.db';
 
-    var db = await openDatabase(databasePath, version: 1, onCreate: _onCreate);
+    var db = await openDatabase(
+      databasePath, 
+      version: 1, 
+      onCreate: _onCreate,
+      password: encrypt("ditonton123")
+    );
     return db;
   }
 
